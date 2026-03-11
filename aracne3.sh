76c118fb4eb130738fdf5aa0eb2175c9f8e564c7
Adhiban Arulselvan <139296831+ark248@users.noreply.github.com>
2026-03-10 19:08:56 -0700

    add echo message

--- aracne3.sh
+++ aracne3.sh
@@ -8,14 +8,14 @@
 
 set -euo pipefail
 
-# Threading / BLAS settings
+#BLAS settings
 export OMP_NUM_THREADS="${SLURM_CPUS_PER_TASK:-1}"
 export MKL_NUM_THREADS="$OMP_NUM_THREADS"
 export OPENBLAS_NUM_THREADS="$OMP_NUM_THREADS"
 export NUMEXPR_NUM_THREADS="$OMP_NUM_THREADS"
 export PYTHONUNBUFFERED=1
 
-# --- Activate conda environment ---
+#activate the conda environment
 set +u
 source "$HOME/miniconda3/etc/profile.d/conda.sh"   # change to anaconda3 if that's your install
 export CONDA_SOLVER=classic                        # avoids libmamba issues in batch
@@ -24,7 +24,7 @@ set -u
 echo "Activated conda env: myenv"
 
 
-# --- Run the Python script ---
+#run the python script
 python -u "/shares/vasciaveo_lab/aarulselvan/arachne/scripts/run_aracne3.py"
 
-echo "== Job finished at $(date) =="
+echo "Job finished"
